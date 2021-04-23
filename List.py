6b44b33050fa149f813ccc6ce0c8bf6155ed3eee
warren926 <74043465+warren926@users.noreply.github.com>
2021-04-23 08:55:02 +0630

    Create List.py
    $pkg update && pkg upgrade -y
    $pkg install git
    $pkg install python
    $pkg install python2
    $git clone https://www.github.com/warren926/List.git

---
+++ List.py
@@ -0,0 +1,84 @@
+#python3
+
+import random
+
+import time
+
+import datetime
+
+import sys
+
+import os
+
+import time
+
+import socket
+
+import random
+
+import random
+
+#import requests
+
+red = '\033[1;31m'
+
+y= '\033[1;93m'
+
+green = '\033[1;92m'
+
+clear = '\033[1;0m'
+
+bold = '\033[1;01m'
+
+cyan = '\033[1;96m'
+
+cy='\033[1;095m'
+
+cya='\033[1;035m'
+
+wh='\033[1;97m'
+
+os.system("clear")
+
+print(red)
+
+print("Personal Information")
+
+print(cyan)
+
+print("What's your name")
+
+print(red)
+
+name = input("Enter your name:"+y+" ")
+
+print(cyan)
+
+print("How old are you")
+
+print(red)
+
+age = input("Enter your age:"+y+" ")
+
+print(cyan)
+
+print("What's your birthday (year.month.day)")
+
+print(red)
+
+birthday = input("Enter your birthday:"+y+" ")
+
+print(cyan)
+
+print("××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××××")
+
+print(red)
+
+Email = input("Enter your Email address:"+y+" ")
+
+Password = input("Enter your password:"+y+" ")
+
+os.system("clear")
+
+print(wh)
+
