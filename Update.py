a92074fb354875b1ad838def13e8d8bdf1522b93
warren926 <74043465+warren926@users.noreply.github.com>
2021-04-23 11:04:32 +0630

    Create Update.py

---
+++ Update.py
@@ -0,0 +1,11 @@
+import os,sys 
+print("1.Update")
+print("2.exit")
+choice = input("Enter: ")
+if choice = 1:
+  os.system("cd $Home")
+  os.system("rm -r List)
+  os.system("git clone https://www.github.com/warren926/List.git")
+elif choice = 2:
+  os.system("cd $Home")
+  os.system("cd List")
